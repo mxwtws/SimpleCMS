c03b74989651837ae32a6c5b4081e0f2d748457e
mxwtws <mxwtws@163.com>
2018-11-30 23:40:38 +0800

    添加ext js应用程序

---
+++ SimpleCMS/Sencha/build/temp/development/SimpleCMS/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\workspace\SimpleCMS\SimpleCMS\Sencha/build/.sass-cache'
---
+++ SimpleCMS/Sencha/build/temp/development/SimpleCMS/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require D:\workspace\SimpleCMS\SimpleCMS\Sencha\app.js
