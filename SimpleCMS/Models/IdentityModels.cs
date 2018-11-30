ccef9c801000930aa29e1a2614aaa40f53b8e23d
mxwtws <mxwtws@163.com>
2018-11-30 20:33:52 +0800

    配置identity和其他配置

---
+++ SimpleCMS/Models/IdentityModels.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SimpleCMS.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // 请注意，authenticationType 必须与 CookieAuthenticationOptions.AuthenticationType 中定义的相应项匹配
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // 在此处添加自定义用户声明
+            return userIdentity;
+        }
+
+        public bool IsApprove { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? LastLogin { get; set; }
+
+        public virtual ICollection<UserProfile> UserProfiles { get; set; }
+    }
+}
